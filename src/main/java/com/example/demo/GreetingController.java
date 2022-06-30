20e2b65100b79ed3ee63bd855b055246a54e697d
Andras Ferenczy <ferenczy.bandi@gmail.com>
2022-06-30 20:23:37 +0200

    Unit testing added

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.demo.Greeting;
 
 import java.util.concurrent.atomic.AtomicLong;
 @RestController
